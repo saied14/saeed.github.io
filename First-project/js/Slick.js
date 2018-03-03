ebfa96ff3a9ecf87e81c373fe2eca0e1978fdbb1
saied14 <37008504+saied14@users.noreply.github.com>
2018-03-03 06:17:40 +0200

    Add files via upload

---
+++ First-project/js/Slick.js
@@ -0,0 +1,24 @@
+/**
+ * Created by Saied on 10/23/2017.
+ */
+
+/*
+ var div1=document.getElementById('div1');
+ var div2=document.getElementById('div2');
+   if (div2.clientHeight>div1.clientHeight)
+      {
+          div2.style.height=div1.clientHeight+'px';
+          console.log("The height of image div is "+images.clientHeight+'px');
+          console.log("The height of paragraph div is "+paragraphs.clientHeight+'px');
+        //  alert("The height of image is equal "+images.clientHeight+" & The height of the paragraph is equal "+paragraphs.clientHeight);
+      }
+      else {
+       paragraphs.style.height=images.clientHeight+'px';
+       console.log("The height of image div is "+images.clientHeight+'px');
+       console.log("The height of paragraph div is "+paragraphs.clientHeight+'px');
+       //   alert("The height of image is equal"+images.clientHeight+" The height of the paragraph is equal "+paragraphs.clientHeight);
+      }
+*/
+$('body').css({
+'overflow': 'hidden'
+});
